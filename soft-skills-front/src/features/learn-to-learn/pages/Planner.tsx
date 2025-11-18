395eca42cdc17cb5a032ae311d70046fea560142
Paoladevelopment <paolala1017@gmail.com>
2025-11-17 23:12:47 -0500

    Add Help page with FAQ and improve date restrictions
    - Add Help page with 13 Q&A about self-regulated learning
    - Style FAQ accordions with secondary color theme
    - Add link to Help page from Planner subtitle
    - Restrict date selectors to future dates only
    - Update UserMenu: remove settings, implement logout
    - Add translations for Help page in English and Spanish

--- soft-skills-front/src/features/learn-to-learn/pages/Planner.tsx
+++ soft-skills-front/src/features/learn-to-learn/pages/Planner.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from '@mui/material'
 import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
 import GoalsSection from '../components/planner/GoalsSection'
 
 const Planner = () => {
@@ -27,7 +28,24 @@ const Planner = () => {
             {t('title')}
           </Typography>
           <Typography variant="subtitle1" component="p">
-            {t('subtitle')}
+            {t('subtitle')}{' '}
+            <Link 
+              to="/learn/help"
+              style={{
+                color: 'inherit',
+                textDecoration: 'underline',
+                textDecorationColor: '#2383E2',
+              }}
+              onMouseEnter={(e) => {
+                e.currentTarget.style.textDecorationColor = '#1565c0'
+              }}
+              onMouseLeave={(e) => {
+                e.currentTarget.style.textDecorationColor = '#2383E2'
+              }}
+            >
+              {t('subtitleLink')}
+            </Link>
+            .
           </Typography>
       </Box>
     
